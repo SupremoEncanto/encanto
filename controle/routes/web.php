00d1f988990b411508a6c0f1cf93981ab7a75777
Tulio Macedo <tulio@localhost.localdomain>
2018-04-12 19:56:20 -0300

    alteração do template bootstrap

--- controle/routes/web.php
+++ controle/routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    //    Route::get('/link1', function ()    {
+//        // Uses Auth Middleware
+//    });
+
+    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
+    #adminlte_routes
+});
