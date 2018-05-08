568ded977e2b6b9c37ac11eebb74fb43b2191b5a
Tulio Macedo <tuliomac@gmail.com>
2018-05-08 00:36:21 -0300

    incluir editar para pessoas

--- controle/resources/views/pessoa/edit.blade.php
+++ controle/resources/views/pessoa/edit.blade.php
@@ -6,53 +6,28 @@
 <div id="tab-general">
     <div class="row mbl">
         <div class="col-lg-12">
-            
-                        <div class="col-md-12">
-                            <div id="area-chart-spline" style="width: 100%; height: 300px; display: none;">
-                            </div>
+            <div class="col-md-12">
+                <div id="area-chart-spline" style="width: 100%; height: 300px; display: none;">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12">    
+            <div class="row">
+                <div class="col-md-12"><h2>Perfil: {{ $pessoa->name }}</h2>
+                    <div class="row mtl">
+                        <div class="col-md-9"> 
+                            <div class="tab-content">
+                                <div class="">
+                                    {!! Form::model($pessoa, ['method' => 'PATCH', 'action' => ['PessoaController@update', $pessoa->id]]) !!}
+                                        @include('pessoa._form', ['submitButton' => 'Editar Pessoa'], ['nome' => 'Editar Pessoa'])
+                                    {!! Form::close() !!}
+                                </div>
+                            </div>    
                         </div>
-            
-        </div>
-
-        <div class="col-lg-12">
-          
-                
-          <div class="row">
-<div class="col-md-12"><h2>Perfil: {{ $pessoa->name }}</h2>
-    <div class="row mtl">
-        <div class="col-md-9">
-            <ul class="nav nav-tabs">
-                <li class="active"><a href="#tab-edit" data-toggle="tab">Editar Perfil</a></li>
-                <li><a href="#tab-messages" data-toggle="tab">Editar Corista</a></li>
-            </ul>
-            <div id="generalTabContent" class="tab-content">
-                <div id="tab-edit" class="tab-pane fade in active">
-                    {!! Form::open(['url' => 'users']) !!}
-                        @include('user._form', ['submitButton' => 'Editar Usuário'], ['nome' => 'Editar Usuário'])
-                    {!! Form::close() !!}
-                </div>
-                <hr/>
-                <div id="tab-edit" class="tab-pane fade in active">
-                        {!! Form::model($pessoa, ['method' => 'PATCH', 'action' => ['PessoaController@update', $pessoa->id]]) !!}
-                            @include('pessoa._form', ['submitButton' => 'Editar Pessoa'], ['nome' => 'Editar Pessoa'])
-                        {!! Form::close() !!}
                     </div>
-                <div id="tab-messages" class="tab-pane fade in">
-                    {!! Form::model($corista, ['method' => 'PATCH', 'action' => ['CoristaController@update', $corista->id]]) !!}
-                        @include('corista._form', ['submitButton' => 'Editar Corista'], ['nome' => 'Editar Corista'])
-                    {!! Form::close() !!}
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-          
-            </div>
-            
-        
- 
-        
+            </div>    
+        </div>    
     </div>
 </div>
 @endsection
