16d8722a9d36ffa2e8f2b09b5b7117e1ba582d46
Tulio Macedo <tulio@localhost.localdomain>
2018-05-08 21:22:48 -0300

    botões de ação das tabelas de dados para as views

--- controle/resources/views/corista/index.blade.php
+++ controle/resources/views/corista/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="col-lg-12">
     <div class="panel panel-grey">
-        <div class="panel-heading">Lista dos Coristas <span class="pull-right"><i class="fa fa-plus"></i>&nbsp;&nbsp;<i class="fa fa-edit"></i>&nbsp;&nbsp;<i class="fa fa-save"></i></span></div>
+        <div class="panel-heading">Lista dos Coristas <span class="pull-right"><i class="fa fa-plus"></i>&nbsp;&nbsp;</span></div>
         <div class="panel-body">
             <table class="table table-hover">
                 <thead>
--- controle/resources/views/ensaio/show.blade.php
+++ controle/resources/views/ensaio/show.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="col-lg-12">
     <div class="panel panel-grey">
-        <div class="panel-heading">Ensaio do dia  {{ Carbon\Carbon::parse($ensaio->date)->toFormattedDateString() }}<span class="pull-right"><i class="fa fa-plus"></i>&nbsp;&nbsp;<i class="fa fa-edit"></i>&nbsp;&nbsp;<i class="fa fa-save"></i></span></div>
+        <div class="panel-heading">Ensaio do dia  {{ Carbon\Carbon::parse($ensaio->date)->toFormattedDateString() }}<span class="pull-right"><i class="fa fa-plus"></i>&nbsp;&nbsp;<i class="fa fa-edit"></i>&nbsp;&nbsp;</span></div>
         <div class="panel-body">
             <table class="table table-hover">
                 <thead>
