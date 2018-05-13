d8ecfc1cbce514ef80d31de9d7a38e02b15d35dd
Tulio Macedo <tuliomac@gmail.com>
2018-05-13 20:58:47 -0300

    alteração do link do footer

--- controle/resources/views/partials/footer.blade.php
+++ controle/resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <!--BEGIN FOOTER-->
 <div id="footer">
     <div class="copyright">
-        <a href="http://themifycloud.com">2014 © KAdmin Responsive Multi-Purpose Template</a>
+        <a href="http://themifycloud.com" target="_blank">2014 © KAdmin Responsive Multi-Purpose Template</a>
     </div>
 </div>
 <!--END FOOTER-->
