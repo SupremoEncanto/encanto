71ce9fe01a6f45604cb29bb78a7c0cb9c166f39d
Tulio Macedo <tuliomac@gmail.com>
2018-06-07 02:30:06 -0300

    alteração do link do site

--- controle/resources/views/layout/index.blade.php
+++ controle/resources/views/layout/index.blade.php
@@ -8,7 +8,7 @@
     <h1 class="display-3">Supremo Encanto!!!</h1>
     <p>Aqui controlamos algumas das informações referentes ao Coral Supremo Encanto.</p>
     <p>O coro é formado por servidores e colaboradores do Supremo Tribunal Federal, convidados e servidores de outros órgãos.</p>
-    <p><a class="btn btn-primary btn-lg" href="http://encanto.tucano.net.br" target="_blank" role="button">Saiba mais &raquo;</a></p>
+    <p><a class="btn btn-primary btn-lg" href="http://www.supremoencanto.art.br" target="_blank" role="button">Saiba mais &raquo;</a></p>
   </div>
 </div>
 @endsection
