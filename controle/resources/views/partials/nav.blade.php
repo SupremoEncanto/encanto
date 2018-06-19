6868abe2b457a80b4391865baab0a4114fe97984
Tulio Macedo <tuliomac@gmail.com>
2018-06-18 23:37:00 -0300

    ajuste na logo nav

--- controle/resources/views/partials/nav.blade.php
+++ controle/resources/views/partials/nav.blade.php
@@ -3,7 +3,7 @@
     <nav id="topbar" role="navigation" style="margin-bottom: 0;" data-step="3" class="navbar navbar-default navbar-static-top">
     <div class="navbar-header">
         <button type="button" data-toggle="collapse" data-target=".sidebar-collapse" class="navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-        <a id="logo" href="/" class="navbar-brand"><span class="fa fa-rocket"></span><span class="logo-text">Encanto</span><span style="display: none" class="logo-text-icon">µ</span></a></div>
+        <a id="logo" href="/" class="navbar-brand"><span class="fa fa-rocket"></span><span class="logo-text">Supremo Encanto</span><span style="display: none" class="logo-text-icon">µ</span></a></div>
     <div class="topbar-main"><a id="menu-toggle" href="#" class="hidden-xs"><i class="fa fa-bars"></i></a> 
         <form id="topbar-search" action="" method="" class="hidden-sm hidden-xs">
             <div class="input-icon right text-white"><a href="#"><i class="fa fa-search"></i></a><input type="text" placeholder="Pesquisar..." class="form-control text-white"/></div>
